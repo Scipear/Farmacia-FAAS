895b1f977042312f14fe494f287ec297056bb058
Anahdev <anadevtools@gmail.com>
2025-02-07 22:10:30 -0400

    migraciones y modelos - Ana

---
+++ app/Models/Empleado.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Empleado extends Model
+{
+    use HasFactory;
+
+    protected $table = 'empleados';
+    protected $fillable = [
+        'cedula',
+        'nombre',
+        'apellido',
+        'correo',
+        'direccion'
+    ];
+
+    //Relacion uno a Muchos
+
+    public function telefonos(){
+    return $this->hasMany(TelefonoEmpleado::class());
+    }
+}
---
+++ app/Models/TelefonoEmpleado.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TelefonoEmpleado extends Model
+{
+    use HasFactory;
+
+    protected $table = 'telefonosEmpleados';
+    protected $fillable =[
+        'empleados_id',
+        'numero'
+    ];
+
+    //Relacion Muchos a uno
+
+    public function empleado(){
+        return $this->belongsTo(Empleado::class());
+    }
+
+}
