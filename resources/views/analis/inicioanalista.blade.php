d75f4d04f31f8d5749f15fef203857b3e602be98
Maria Navarro <mariadvnr04@gmail.com>
2025-03-08 23:25:55 -0400

    vista de iincio de analista de compras

--- resources/views/analis/inicioanalista.blade.php
+++ resources/views/analis/inicioanalista.blade.php
@@ -2,19 +2,30 @@
 
 @section('titulo', 'Analista de Compras inicio')
 
+<header>
+    @yield('header', 'Farmacias FAAS')
+    <ul class="nav-tabs"> <!-- Pestañas dentro del header -->
+            <li class="nav-item">
+                <a class="nav-link active" href="/analis/inicioanalista">Inicio</a>
+            </li>
+            <li class="nav-item">
+                <a  class="nav-link active" href="/admin/logout">Cerrar Sesión</a>
+            </li>
+    </ul>
+</header>
+
 @section('contenido')
 
-        <h1>Bienvenido Analita de compras</h1>
-        <p>Selecciona una opcion para gestionar</p><br>
-        <b>Realizar Pedido</b>
-        <button>Ir</button><br><br>
-        <b>Consultar pedidos</b>
-        <button>Ir</button><br><br>
-        <b>Consultar compras</b>
-        <button>Ir</button><br><br>
+        <div class="container">
+            <h1>Bienvenido al Panel de Analita de compras</h1>
+            <p>¡Has iniciado sesión como Analita de compras!</p>
+        </div>
+
+        <b>Gestionar pedidos</b>
+        <a  class="botonIr" href="">Ir</a><br><br><br>
+        <b>Gestionar compras</b>
+        <a  class="botonIr" href="">Ir</a><br><br><br>
         <b>Ver cuentas por pagar</b>
-        <button>Ir</button><br><br>
-        <b>Cerrar sesión</b>
-        <button>Ir</button><br><br>
+        <a  class="botonIr" href="">Ir</a><br><br><br>
         
 @endsection
