ec4ddf93c98c112164c1dcf671e7993b6f155ba2
Scipear <Pantauwu09@gmail.com>
2025-03-10 06:52:23 -0400

    otro más

--- resources/views/farmaceutico/medicinaSucursales.blade.php
+++ resources/views/farmaceutico/medicinaSucursales.blade.php
@@ -24,7 +24,6 @@
         <div class="container">
         </div>
 
-        <a class="botonAg" href="/farmaceutico/{{$medicina->id}}/formMedicinaSucursal">Agregar +</a>
         <center>
             <table>
                 <tr>
