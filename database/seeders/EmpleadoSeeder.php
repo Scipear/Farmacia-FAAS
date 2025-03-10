0777a794f763198b0fa341c464060caf4e93a8fd
Scipear <Pantauwu09@gmail.com>
2025-03-10 04:04:16 -0400

    seeders editados

--- database/seeders/EmpleadoSeeder.php
+++ database/seeders/EmpleadoSeeder.php
@@ -21,7 +21,7 @@ class EmpleadoSeeder extends Seeder
                 'cedula' => '12248585',
                 'nombre' => 'Rosa',
                 'apellido' => 'Perales',
-                'correo' => 'administrador@mail.com',
+                'correo' => 'administrador1@mail.com',
                 'direccion' => 'Calle 123',
             ],
 
@@ -29,7 +29,7 @@ class EmpleadoSeeder extends Seeder
                 'cedula' => '25853499',
                 'nombre' => 'Carlos',
                 'apellido' => 'Rojas',
-                'correo' => 'gerente@mail.com',
+                'correo' => 'gerente1@mail.com',
                 'direccion' => 'Calle 321',
             ],
 
@@ -37,7 +37,7 @@ class EmpleadoSeeder extends Seeder
                 'cedula' => '35478892',
                 'nombre' => 'Luis',
                 'apellido' => 'Garcia',
-                'correo' => 'farmaceutico@mail.com',
+                'correo' => 'farmaceutico1@mail.com',
                 'direccion' => 'Calle 789',
             ],
 
@@ -45,18 +45,51 @@ class EmpleadoSeeder extends Seeder
                 'cedula' => '7589248',
                 'nombre' => 'Maria',
                 'apellido' => 'Gonzales',
-                'correo' => 'analista@mail.com',
+                'correo' => 'analista1@mail.com',
                 'direccion' => 'Calle 5464',
+            ],
+
+            [
+                'cedula' => '456412',
+                'nombre' => 'Betty',
+                'apellido' => 'Del Rey',
+                'correo' => 'administrador2@mail.com',
+                'direccion' => 'Calle 85996',
+            ],
+
+            [
+                'cedula' => '333333',
+                'nombre' => 'Lorenzo',
+                'apellido' => 'Sifontes',
+                'correo' => 'gerente2@mail.com',
+                'direccion' => 'Calle 78645',
+            ],
+
+            [
+                'cedula' => '6657852',
+                'nombre' => 'Mina',
+                'apellido' => 'Rin',
+                'correo' => 'farmaceutico2@mail.com',
+                'direccion' => 'Loma Linda',
+            ],
+
+            [
+                'cedula' => '28611',
+                'nombre' => 'Victor',
+                'apellido' => 'Vasquez',
+                'correo' => 'analista2@mail.com',
+                'direccion' => 'Villa Betania',
             ]
         ];
 
         $i = 1;
+        $j = 1;
         foreach($empleados as $empleadoData){
             $sucursalId = rand(1, 10);
 
             $empleado = Empleado::create($empleadoData);
             $empleado->cargos()->attach($i);
-            $empleado->sucursales()->attach($sucursalId);
+            $empleado->sucursales()->attach($j);
             $empleado->telefonos()->create([
                 'empleado_id' => $empleado->id,
                 'tipo' => 'Personal',
@@ -74,6 +107,11 @@ class EmpleadoSeeder extends Seeder
             ]);
 
             $i = $i + 1;
+
+            if($i == 4){
+                $i = 1;
+                $j = $j + 1;
+            }
         }
 
         $empleadosFactory = Empleado::factory(50)->create();
